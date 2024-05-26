61ed53d9436e17397037a12c49f7141e53c18558
maxefimov <efimov.top.academy@gmail.com>
2024-05-26 11:51:42 +0300

    Добавьте файлы проекта.

---
+++ WpfResourceApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfResourceApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btn.FontSize = 28;
+            btn.Content = "Press me";
+
+            btn.Foreground = this.TryFindResource("brushRed") as Brush;
+
+            LinearGradientBrush grad = new();
+            grad.GradientStops.Add(new(Colors.Pink, 0));
+            grad.GradientStops.Add(new(Colors.Yellow, 1));
+
+            this.Resources.Add("brushGrad", grad);
+            btn.Background = this.TryFindResource("brushGrad") as Brush;
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["brushRed"] = new SolidColorBrush(Colors.Blue);
+        }
+    }
+}
